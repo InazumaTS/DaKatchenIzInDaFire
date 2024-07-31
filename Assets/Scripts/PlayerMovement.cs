e1af8641bba4391c70813730ecc318291d905c41
FlakySeaSalt <91329476+InazumaTS@users.noreply.github.com>
2024-07-31 15:37:04 +0530

    Added Collisions
    Gave The Player the Ability to interact with Colliders without using RigidBody

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -11,14 +11,43 @@ public class PlayerMovement : MonoBehaviour
     private bool IsWalking = false;
     [SerializeField]
     private PlayerInput input;
+    [SerializeField]
+    private float PlayerRadiusSize = 0.7f;
+    [SerializeField]
+    private float PlayerHeight = 2f;
     private void Update()
     {
         Vector2 PlayerMove = input.GetMovementVectorNormalized();
         Vector3 ProperPlayerMove = new Vector3(PlayerMove.x,0,PlayerMove.y);
         IsWalking = ProperPlayerMove != Vector3.zero;
-        transform.position += ProperPlayerMove * Time.deltaTime * MoveSpeed;
+
+        CollisionDetection(ProperPlayerMove);
+
         float RotationSpeed = 10f;
         transform.forward = Vector3.Slerp(transform.forward, ProperPlayerMove, Time.deltaTime * RotationSpeed);
+    }
+
+    private void CollisionDetection(Vector3 ProperPlayerMove)
+    {
+        bool canMove = Physics.CapsuleCast(transform.position, transform.position + Vector3.up * PlayerHeight, PlayerRadiusSize, ProperPlayerMove, Time.deltaTime * MoveSpeed);
+        if (canMove)
+        {
+            Vector3 ProperPlayerMoveX = new Vector3(ProperPlayerMove.x, 0, 0);
+            canMove = Physics.CapsuleCast(transform.position, transform.position + Vector3.up * PlayerHeight, PlayerRadiusSize, ProperPlayerMoveX, Time.deltaTime * MoveSpeed);
+            if (!canMove)
+                ProperPlayerMove = ProperPlayerMoveX;
+            else
+            {
+                ProperPlayerMoveX = new Vector3(0, 0, ProperPlayerMove.z);
+                canMove = Physics.CapsuleCast(transform.position, transform.position + Vector3.up * PlayerHeight, PlayerRadiusSize, ProperPlayerMoveX, Time.deltaTime * MoveSpeed);
+                if (!canMove)
+                    ProperPlayerMove = ProperPlayerMoveX;
+
+            }
+
+        }
+        if (!canMove)
+            transform.position += ProperPlayerMove * Time.deltaTime * MoveSpeed;
     }
 
     public bool isWalking()
